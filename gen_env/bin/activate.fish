1721c0287c047ea4fae1b6f3478e6826aac471b6
Sagar F. Lokhande <sagar.f.lokhande@gmail.com>
2022-10-16 21:10:54 +0530

    Added files for the user to inpur data.

--- gen_env/bin/activate.fish
+++ gen_env/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/sagar/Desktop/local_repos/invoice_generator/gen_env"
+set -gx VIRTUAL_ENV "/Users/sagar/Desktop/git_repos/invoice_generator/gen_env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
